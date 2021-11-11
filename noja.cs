8cb11e99d39f9dff427c7f81b1855e760ac076bf
nigams <snigamanandhasarma@gmail.com>
2021-11-11 17:53:48 +0530

    Offline addition of noja file

---
+++ noja.cs
@@ -0,0 +1,13 @@
+using System;
+using static System.Console;
+
+namespace NigamOnlineCheck
+{
+    class Program
+    {
+        public void main (string[]args)
+        {
+            WriteLine("Enna koduma saravanan");
+        }
+    }
+}
